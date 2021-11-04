3929a910af92be87de54bffd827b385363874260
amandaesmith3 <amanda.s@ionic.io>
2021-11-04 09:09:35 -0500

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public alertController: AlertController) {}
 
-  constructor() {}
+  async presentPrompt(handleConfirm, handleCancel) {
+    const alert = await this.alertController.create({
+      header: 'Create Todo',
+      message: 'Please enter the todo name.',
+      inputs: [
+        {
+          type: 'text',
+          name: 'todo'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: handleCancel
+        },
+        {
+          text: 'Submit',
+          handler: handleConfirm
+        }
+      ]
+    });
 
+    alert.present();
+  }
+
+  async handleChange(e) {
+    if(e.detail.checked) {
+      this.presentPrompt(data => {
+        const todoName = data.todo;
+        console.log('Todo submitted:', todoName);
+        if(!todoName) {
+          e.target.checked = false;
+        }
+      }, () => {
+        console.log('Prompt cancelled');
+        e.target.checked = false;
+      });
+    }
+  }
 }
